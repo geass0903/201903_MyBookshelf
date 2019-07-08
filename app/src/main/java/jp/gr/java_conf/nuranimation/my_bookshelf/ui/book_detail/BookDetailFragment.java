8b6b62e3707f98c4eb6aa53f932ec271d1599186
Kamada <kamata@pulsetec.co.jp>
2019-07-08 10:00:06 +0900

    change 20190708

--- app/src/main/java/jp/gr/java_conf/nuranimation/my_bookshelf/ui/book_detail/BookDetailFragment.java
+++ app/src/main/java/jp/gr/java_conf/nuranimation/my_bookshelf/ui/book_detail/BookDetailFragment.java
@@ -8,7 +8,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
@@ -28,29 +27,29 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.drawee.view.SimpleDraweeView;
-
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+import jp.gr.java_conf.nuranimation.my_bookshelf.R;
 import jp.gr.java_conf.nuranimation.my_bookshelf.model.database.MyBookshelfDBOpenHelper;
+import jp.gr.java_conf.nuranimation.my_bookshelf.model.entity.BookData;
+import jp.gr.java_conf.nuranimation.my_bookshelf.model.entity.SpinnerItem;
 import jp.gr.java_conf.nuranimation.my_bookshelf.model.utils.BookDataUtils;
 import jp.gr.java_conf.nuranimation.my_bookshelf.model.utils.CalendarUtils;
 import jp.gr.java_conf.nuranimation.my_bookshelf.ui.MyBookshelfEvent;
-import jp.gr.java_conf.nuranimation.my_bookshelf.R;
 import jp.gr.java_conf.nuranimation.my_bookshelf.ui.ReadStatusSpinnerArrayAdapter;
-import jp.gr.java_conf.nuranimation.my_bookshelf.model.entity.SpinnerItem;
-import jp.gr.java_conf.nuranimation.my_bookshelf.model.entity.BookData;
+import jp.gr.java_conf.nuranimation.my_bookshelf.ui.base.BaseFragment;
 import jp.gr.java_conf.nuranimation.my_bookshelf.ui.dialog.NormalDatePicker;
 import jp.gr.java_conf.nuranimation.my_bookshelf.ui.dialog.NormalDialogFragment;
-import jp.gr.java_conf.nuranimation.my_bookshelf.ui.base.BaseFragment;
 
 
 public class BookDetailFragment extends BaseFragment implements NormalDatePicker.OnBaseDateSetListener, NormalDialogFragment.OnNormalDialogListener {
     public static final String TAG = BookDetailFragment.class.getSimpleName();
     private static final boolean D = true;
 
-    private static final String TAG_FINISH_READ_DATE_PICKER = "PermissionsFragment.TAG_FINISH_READ_DATE_PICKER";
+    private static final String TAG_DATE_PICKER = "BookDetailFragment.TAG_DATE_PICKER";
+    private static final String TAG_CLEAR_DATE_DIALOG = "BookDetailFragment.TAG_CLEAR_DATE_DIALOG";
 
     private static final int REQUEST_CODE_FINISH_READ_DATE = 101;
 
@@ -59,10 +58,10 @@ public class BookDetailFragment extends BaseFragment implements NormalDatePicker
     public static final String KEY_BOOK_DATA = "BookDetailFragment.KEY_BOOK_DATA";
 
     private MyBookshelfDBOpenHelper mDBOpenHelper;
-    private ReadStatusSpinnerArrayAdapter mArrayAdapter;
+
 
     private BookData bookData;
-
+    private ReadStatusSpinnerArrayAdapter mArrayAdapter;
     private SimpleDraweeView sdv_bookImage;
     private EditText et_title;
     private EditText et_author;
@@ -73,8 +72,6 @@ public class BookDetailFragment extends BaseFragment implements NormalDatePicker
     private TextView tv_finishReadDate;
     private RatingBar rb_rating;
     private Spinner sp_readStatus;
-    private Button bt_downloadBookData;
-    private Button bt_link_rakutenBooks;
 
     @Override
     public void onAttach (Context context) {
@@ -102,7 +99,7 @@ public class BookDetailFragment extends BaseFragment implements NormalDatePicker
             }
         }
         initView(view);
-        initView(view, bookData);
+        setBookDataToView(bookData);
     }
 
     @Override
@@ -179,6 +176,7 @@ public class BookDetailFragment extends BaseFragment implements NormalDatePicker
     public void onNormalDialogSucceeded(int requestCode, int resultCode, Bundle params) {
         if(resultCode == DialogInterface.BUTTON_POSITIVE){
             if (requestCode == REQUEST_CODE_FINISH_READ_DATE) {
+                bookData.setFinishReadDate("");
                 tv_finishReadDate.setText(getString(R.string.label_no_data));
             }
         }
@@ -204,9 +202,11 @@ public class BookDetailFragment extends BaseFragment implements NormalDatePicker
         tv_finishReadDate = view.findViewById(R.id.book_detail_finish_read_date);
         sp_readStatus = view.findViewById(R.id.book_detail_spinner_read_status);
         rb_rating = view.findViewById(R.id.book_detail_rating_bar);
-        bt_downloadBookData = view.findViewById(R.id.book_detail_button_download_book_data);
-        bt_link_rakutenBooks = view.findViewById(R.id.book_detail_button_rakutenUrl);
+        Button bt_downloadBookData = view.findViewById(R.id.book_detail_button_download_book_data);
+        Button bt_link_rakutenBooks = view.findViewById(R.id.book_detail_button_rakutenUrl);
 
+        sdv_bookImage.setOnClickListener(mOnClickListener);
+        sdv_bookImage.setOnLongClickListener(mOnLongClickListener);
         et_title.addTextChangedListener(new GenericTextWatcher(et_title));
         et_author.addTextChangedListener(new GenericTextWatcher(et_author));
         et_publisher.addTextChangedListener(new GenericTextWatcher(et_publisher));
@@ -216,19 +216,94 @@ public class BookDetailFragment extends BaseFragment implements NormalDatePicker
         sp_readStatus.setAdapter(mArrayAdapter);
         sp_readStatus.setOnItemSelectedListener(mOnItemSelectedListener);
         rb_rating.setOnRatingBarChangeListener(mOnRatingBarChangeListener);
+        bt_downloadBookData.setOnClickListener(mOnClickListener);
+        bt_link_rakutenBooks.setOnClickListener(mOnClickListener);
     }
 
-
-
+    private void setBookDataToView(BookData bookData) {
+        if (bookData != null) {
+            if (sdv_bookImage != null) {
+                String urlString = BookDataUtils.parseUrlString(bookData.getImage(), BookDataUtils.IMAGE_TYPE_LARGE);
+                if (!TextUtils.isEmpty(urlString) && sdv_bookImage != null) {
+                    sdv_bookImage.setImageURI(Uri.parse(urlString));
+                }
+            }
+            if (tv_isbn != null) {
+                if (!TextUtils.isEmpty(bookData.getISBN())) {
+                    tv_isbn.setText(bookData.getISBN());
+                }
+            }
+            if (tv_salesDate != null) {
+                if (!TextUtils.isEmpty(bookData.getSalesDate())) {
+                    tv_salesDate.setText(bookData.getSalesDate());
+                } else {
+                    tv_salesDate.setText(R.string.label_unknown);
+                }
+            }
+            if (tv_itemPrice != null) {
+                if (!TextUtils.isEmpty(bookData.getItemPrice())) {
+                    String itemPrice = getString(R.string.label_price_yen_mark) + bookData.getItemPrice();
+                    tv_itemPrice.setText(itemPrice);
+                } else {
+                    tv_itemPrice.setText(R.string.label_unknown);
+                }
+            }
+            if (et_title != null) {
+                if (!TextUtils.isEmpty(bookData.getTitle())) {
+                    et_title.setText(bookData.getTitle());
+                }
+            }
+            if (et_author != null) {
+                if (!TextUtils.isEmpty(bookData.getAuthor())) {
+                    et_author.setText(bookData.getAuthor());
+                }
+            }
+            if (et_publisher != null) {
+                if (!TextUtils.isEmpty(bookData.getPublisher())) {
+                    et_publisher.setText(bookData.getPublisher());
+                }
+            }
+            if (tv_finishReadDate != null) {
+                if (!TextUtils.isEmpty(bookData.getFinishReadDate())) {
+                    tv_finishReadDate.setText(bookData.getFinishReadDate());
+                } else {
+                    tv_finishReadDate.setText(R.string.label_no_data);
+                }
+            }
+            if (sp_readStatus != null && mArrayAdapter != null) {
+                if (!TextUtils.isEmpty(bookData.getReadStatus())) {
+                    int position = mArrayAdapter.getPosition(bookData.getReadStatus());
+                    sp_readStatus.setSelection(position);
+                }
+            }
+            if (rb_rating != null) {
+                if (!TextUtils.isEmpty(bookData.getRating())) {
+                    float rating = BookDataUtils.convertRating(bookData.getRating());
+                    rb_rating.setRating(rating);
+                }
+            }
+        }
+    }
 
 
     private View.OnClickListener mOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             int id = v.getId();
-            if (id == R.id.book_detail_finish_read_date) {
-                String readDate = tv_finishReadDate.getText().toString();
-                showDatePicker(REQUEST_CODE_FINISH_READ_DATE, readDate);
+            switch (id){
+                case R.id.book_detail_image:
+                    if(D) Log.d(TAG,"onClick");
+                    break;
+                case R.id.book_detail_finish_read_date:
+                    String readDate = tv_finishReadDate.getText().toString();
+                    showDatePicker(REQUEST_CODE_FINISH_READ_DATE, readDate);
+                    break;
+                case R.id.book_detail_button_download_book_data:
+                    if(D) Log.d(TAG,"onClick");
+                    break;
+                case R.id.book_detail_button_rakutenUrl:
+                    if(D) Log.d(TAG,"onClick");
+                    break;
             }
         }
     };
@@ -237,9 +312,13 @@ public class BookDetailFragment extends BaseFragment implements NormalDatePicker
         @Override
         public boolean onLongClick(View v) {
             int id = v.getId();
-            if (id == R.id.book_detail_finish_read_date) {
-                showClearDateDialog(REQUEST_CODE_FINISH_READ_DATE);
-                return true;
+            switch (id){
+                case R.id.book_detail_image:
+                    if(D) Log.d(TAG,"onLongClick");
+                    return true;
+                case R.id.book_detail_finish_read_date:
+                    showClearDateDialog(REQUEST_CODE_FINISH_READ_DATE);
+                    return true;
             }
             return false;
         }
@@ -259,7 +338,6 @@ public class BookDetailFragment extends BaseFragment implements NormalDatePicker
         public void onNothingSelected(AdapterView<?> adapter) {
         }
     };
-
 
     private RatingBar.OnRatingBarChangeListener mOnRatingBarChangeListener = new RatingBar.OnRatingBarChangeListener() {
         @Override
@@ -282,140 +360,20 @@ public class BookDetailFragment extends BaseFragment implements NormalDatePicker
         bundle.putInt(NormalDatePicker.KEY_MONTH, calendar.get(Calendar.MONTH));
         bundle.putInt(NormalDatePicker.KEY_DAY_OF_MONTH, calendar.get(Calendar.DAY_OF_MONTH));
         bundle.putInt(NormalDatePicker.KEY_REQUEST_CODE, requestCode);
-        NormalDialogFragment.showNormalDialog(this, bundle, TAG_FINISH_READ_DATE_PICKER);
+        NormalDialogFragment.showNormalDialog(this, bundle, TAG_DATE_PICKER);
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    private void initView(View view,BookData book) {
-
-    }
-
-
-    private void setBookData(BookData book){
-
-        if(book != null) {
-            sdv_bookImage.setImageURI(Uri.parse(BookDataUtils.parseUrlString(book.getImage(),BookDataUtils.IMAGE_TYPE_LARGE)));
-            BookDataUtils.parseUrlString(book.getImage());
-            sdv_bookImage.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if(D) Log.d(TAG,"onClick");
-
-
-
-                }
-            });
-
-            sdv_bookImage.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View v) {
-                    if(D) Log.d(TAG,"onLongClick");
-                    return true;
-                }
-            });
-
-
-
-            if(D) Log.d(TAG,"setTitle" + book.getTitle());
-            et_title.setText(book.getTitle());
-
-
-
-
-            if(D) Log.d(TAG,"setAuthor" + book.getAuthor());
-            et_author.setText(book.getAuthor());
-
-            if(D) Log.d(TAG,"setPublisher" + book.getPublisher());
-            et_publisher.setText(book.getPublisher());
-
-
-            if(!TextUtils.isEmpty(book.getSalesDate())) {
-                tv_salesDate.setText(book.getSalesDate());
-            }
-            tv_itemPrice.setText(book.getItemPrice());
-            tv_isbn.setText(book.getISBN());
-            if(!TextUtils.isEmpty(book.getFinishReadDate())) {
-                tv_finishReadDate.setText(book.getFinishReadDate());
-            }
-            sp_readStatus.setSelection(mArrayAdapter.getPosition(book.getReadStatus()));
-            rb_rating.setRating(BookDataUtils.convertRating(book.getRating()));
-        }
-    }
-
-
-    private BookData getBookData(){
-        BookData book = new BookData(bookData);
-//        book.setImage(bookData.getImage());
-//        book.setTitle(et_title.getText().toString());
-//        book.setAuthor(et_author.getText().toString());
-//        book.setPublisher(et_publisher.getText().toString());
-//        book.setSalesDate(tv_salesDate.getText().toString());
-//        book.setItemPrice(tv_itemPrice.getText().toString());
-//        book.setISBN(tv_isbn.getText().toString());
-//        book.setFinishReadDate(tv_finishReadDate.getText().toString());
-//        SpinnerItem item = (SpinnerItem)sp_readStatus.getSelectedItem();
-//        book.setReadStatus(item.getCode());
-//        book.setRating(BookDataUtils.convertRating(rb_rating.getRating()));
-
-
-        if(D) Log.d(TAG,"" + book.getTitle());
-        if(D) Log.d(TAG,"" + book.getAuthor());
-        if(D) Log.d(TAG,"" + book.getPublisher());
-        if(D) Log.d(TAG,"" + book.getSalesDate());
-        if(D) Log.d(TAG,"" + book.getReadStatus());
-
-        return book;
-    }
-
-
-
-
-
-
-
-
-
-
-
+    @SuppressWarnings("SameParameterValue")
     private void showClearDateDialog(int requestCode) {
-        if (getActivity() != null) {
-            Bundle bundle = new Bundle();
-            bundle.putString(NormalDialogFragment.KEY_TITLE, getString(R.string.dialog_title_clear_date));
-            bundle.putString(NormalDialogFragment.KEY_MESSAGE, getString(R.string.dialog_message_clear_date));
-            bundle.putString(NormalDialogFragment.KEY_POSITIVE_LABEL, getString(R.string.dialog_button_label_positive));
-            bundle.putString(NormalDialogFragment.KEY_NEGATIVE_LABEL, getString(R.string.dialog_button_label_negative));
-            bundle.putInt(NormalDialogFragment.KEY_REQUEST_CODE, requestCode);
-            bundle.putBoolean(NormalDialogFragment.KEY_CANCELABLE, true);
-            FragmentManager manager = getActivity().getSupportFragmentManager();
-            NormalDialogFragment fragment = NormalDialogFragment.newInstance(this, bundle);
-            fragment.show(manager, NormalDialogFragment.TEMP_TAG);
-        }
+        Bundle bundle = new Bundle();
+        bundle.putString(NormalDialogFragment.KEY_TITLE, getString(R.string.dialog_title_clear_date));
+        bundle.putString(NormalDialogFragment.KEY_MESSAGE, getString(R.string.dialog_message_clear_date));
+        bundle.putString(NormalDialogFragment.KEY_POSITIVE_LABEL, getString(R.string.dialog_button_label_positive));
+        bundle.putString(NormalDialogFragment.KEY_NEGATIVE_LABEL, getString(R.string.dialog_button_label_negative));
+        bundle.putInt(NormalDialogFragment.KEY_REQUEST_CODE, requestCode);
+        bundle.putBoolean(NormalDialogFragment.KEY_CANCELABLE, true);
+        NormalDialogFragment.showNormalDialog(this,bundle, TAG_CLEAR_DATE_DIALOG);
     }
-
-
-
-
 
     private List<SpinnerItem> getSpinnerItem_ReadStatus() {
         List<SpinnerItem> list = new ArrayList<>();
@@ -426,8 +384,6 @@ public class BookDetailFragment extends BaseFragment implements NormalDatePicker
         list.add(new SpinnerItem(BookData.STATUS_NONE,          getString(R.string.read_status_label_5)));
         return list;
     }
-
-
 
     private class GenericTextWatcher implements TextWatcher {
         private View view;
@@ -465,7 +421,7 @@ public class BookDetailFragment extends BaseFragment implements NormalDatePicker
             }
 
         }
-        
+
         private void confirmString(View view, String text){
             if(D) Log.d(TAG, "Confirm: " + text);
             switch(view.getId()){
@@ -481,7 +437,6 @@ public class BookDetailFragment extends BaseFragment implements NormalDatePicker
             }
         }
     }
-
 
 
 }
